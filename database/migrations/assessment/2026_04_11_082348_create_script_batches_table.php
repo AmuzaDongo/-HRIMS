6f779bf6a5bbd80eed1a71d708bcda65bc050749
Dongo <dongoamuza@gmail.com>
2026-04-11 11:26:04 +0300

    add script_batches migration

---
+++ database/migrations/assessment/2026_04_11_082348_create_script_batches_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('script_batches', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignId('paper_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('center_id')->constrained('marking_centers')->cascadeOnDelete();
+            $table->string('batch_code')->unique();
+            $table->integer('total_scripts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('script_batches');
+    }
+};
